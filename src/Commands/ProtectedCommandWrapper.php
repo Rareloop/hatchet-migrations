76623a28c426412e4681f4364ecd59f4c982e296
Joe Lambert <joe@rareloop.com>
2020-04-17 11:40:38 +0100

    Add `—force` flag to supress warnings

--- src/Commands/ProtectedCommandWrapper.php
+++ src/Commands/ProtectedCommandWrapper.php
@@ -2,15 +2,25 @@
 
 namespace Rareloop\Hatchet\Migrations\Commands;
 
+use Rareloop\Lumberjack\Application;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 class ProtectedCommandWrapper extends CommandWrapper
 {
+    public function __construct(Application $app, Command $command, string $signature, string $description = null)
+    {
+        parent::__construct($app, $command, $signature, $description);
+
+        $this->addOption('force', 'f', InputOption::VALUE_NONE, 'Supress confirmation on production environments');
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if ($this->isAProductionEnvironment()) {
+        if ($this->isAProductionEnvironment() && !$input->getOption('force')) {
             $helper = $this->getHelper('question');
             $output->writeln('<comment>**********************************</comment>');
             $output->writeln('<comment>*   Application in Production!   *</comment>');
