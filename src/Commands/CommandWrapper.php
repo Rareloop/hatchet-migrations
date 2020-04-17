4afb2b22abb4503ba9c29492c695deb2400dba98
Joe Lambert <joe@rareloop.com>
2020-04-17 12:16:12 +0100

    Remove unused Phinx options from Hatchet commands

--- src/Commands/CommandWrapper.php
+++ src/Commands/CommandWrapper.php
@@ -7,6 +7,8 @@ use Rareloop\Hatchet\Commands\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\InputDefinition;
 
 class CommandWrapper extends Command
 {
@@ -20,11 +22,41 @@ class CommandWrapper extends Command
         parent::__construct($app);
 
         $this->setDescription($description ?: $command->getDescription());
-        $this->setDefinition($command->getDefinition());
+        $this->setDefinition($this->buildDefinitionFromCommand($command));
+    }
+
+    protected function buildDefinitionFromCommand(SymfonyCommand $command): InputDefinition
+    {
+        $commandDefinition = $command->getDefinition();
+        $thisDefinition = new InputDefinition();
+        $thisDefinition->setArguments($commandDefinition->getArguments());
+
+        $options = $commandDefinition->getOptions();
+
+        // Strip options that we do not want to expose to the Hatchet Command
+        unset($options['environment']);
+        unset($options['parser']);
+        unset($options['configuration']);
+
+        $thisDefinition->setOptions($options);
+
+        return $thisDefinition;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->command->execute($input, $output);
+        // As we've removed some options we need to make sure that these are present in the $input that
+        // we provide to the actual command. We rebuild the $input from the arguments passed in and also
+        // force the environment to be set to suppress the Phinx warning
+
+        $combinedDefinition = $this->command->getDefinition();
+        $combinedDefinition->addOptions($this->getDefinition()->getOptions());
+
+        $argv = explode(' ', $input->__toString());
+        array_push($argv, '--environment=wp');
+
+        $newInput = new ArgvInput($argv, $combinedDefinition);
+
+        $this->command->execute($newInput, $output);
     }
 }
